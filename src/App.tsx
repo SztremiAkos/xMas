54fa6ceed7cf28a6b440448220a00686f2839b56
Sztremi Akos <sz.akos0101@gmail.com>
2022-11-30 21:47:39 +0100

    WIP

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import './App.css';
+import useQuestions from "./hooks/useQuestions";
+
+function App() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0)
+  const [showVid, onShowVid] = useState(false)
+  const {questions} = useQuestions();
+  const handleAnswer = (isCorrect: boolean) => {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) setCurrentQuestion(nextQuestion)
+    else onShowVid(true);
+  }
+  const handleReset = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+  }
+  return (
+    <>
+      <div className="reset">
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <div className="outer">
+        <div className='app'>
+          {showVid ? (
+            <div className='score-section'>
+              You scored {score} out of {questions.length}
+            </div>
+          ) : (
+            <>
+              <div className='question-count'><span>{questions[currentQuestion].questionText}</span></div>
+              <div className='answer-section'>
+                {questions[currentQuestion].answerOptions.map((answerOption) => (
+                  <button onClick={() => handleAnswer(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useQuestions.ts
@@ -0,0 +1,33 @@
+﻿const useQuestions = () =>{
+  const questions = [
+    {
+      questionText: "Melyik állatot hagytuk ki az állatkertben?",
+      answerOptions: [
+        {answerText: 'Cápa', isCorrect: true},
+        {answerText: 'Oroszlán', isCorrect: false},
+        {answerText: 'Fóka', isCorrect: false},
+        {answerText: 'Teve', isCorrect: false},
+      ],
+    },
+    {
+      questionText: "Milyen volt az első pizza amit sütöttünk?",
+      answerOptions: [
+        {answerText: 'tökéletes', isCorrect: false},
+        {answerText: 'égett', isCorrect: false},
+        {answerText: 'csúnya de finom', isCorrect: true},
+        {answerText: 'tejfölös', isCorrect: false},
+      ],
+    },
+    {
+      questionText: "third",
+      answerOptions: [
+        {answerText: 'first', isCorrect: true},
+        {answerText: '2nd', isCorrect: false},
+        {answerText: '3rd', isCorrect: false},
+        {answerText: '4th', isCorrect: false},
+      ],
+    },
+  ]
+  return {questions};
+}
+export default useQuestions;
