2e36ce22f4c177ed2d8262ebc174280035422dcb
Sztremi Akos <sz.akos0101@gmail.com>
2022-12-20 10:24:19 +0100

    questions are missing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Page from "./sources/Page";
 
-function App() {
-  
-  return <Page />  ;
-}
+const App = () => <Page/>;
 
 export default App;
--- src/sources/Video.tsx
+++ src/sources/Video.tsx
@@ -1,10 +1,13 @@
 ﻿const Video = () => {
+  const src = "https://www.youtube.com/embed/5wu1PAE7NH0"
+  const allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
   return (
     <iframe 
       width="560" 
-      height="315" 
-      src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      height="315"
+      src={src}
+      title="Panna Final gift"
+      allow={allow}
       allowFullScreen
     />
   );
